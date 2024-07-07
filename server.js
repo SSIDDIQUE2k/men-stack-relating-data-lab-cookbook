84d64f3f8e7b0ca6c36312e040815e109ad11615
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-07-07 18:38:35 -0400

    build a navbar

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+// server.js
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/foods.js'); //added foods controller
 
@@ -44,8 +48,10 @@ app.get('/vip-lounge', (req, res) => {
   }
 });
 
+app.use(passUserToView)
 app.use('/auth', authController);
-app.use('/users/:userId/foods',foodsController); //added foods controller 
+app.use(isSignedIn);
+app.use('/users/:userId/foods',foodsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
